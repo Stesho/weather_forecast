c86a8bf61b48c6a1c93705f3b940084d05d31127
Steshenko Dmitriy <3301297@mail.ru>
2021-08-08 18:20:44 +0300

    The foundation

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let temp = document.querySelector('.head-location');
+
+window.addEventListener('load', () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => { 
+            let long = position.coords.longitude;
+            let lat = position.coords.latitude; 
+            let apiKey = '8d410c4d25751a2ffc588b6019d83a8b';
+            let part = 'current';
+
+            console.log(cities.findIndex(item => item.name === 'Minsk' ? true : false));
+            //const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=${part}&appid=${apiKey}`;
+            // const api = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=${apiKey}`;
+            // fetch(api)
+            //     .then(response => {
+            //         return response.json()
+            //     })
+            //     .then(data => {
+            //         console.log(data)
+            //     });
+        });
+    }
+});
+
+
