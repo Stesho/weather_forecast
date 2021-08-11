40e877d2bf8e014503b01787f50da66d6c527cd3
Steshenko Dmitriy <3301297@mail.ru>
2021-08-11 21:05:48 +0300

    Add icons; add design features

--- js/app.js
+++ js/app.js
@@ -8,19 +8,19 @@
 //             let apiKey = '8d410c4d25751a2ffc588b6019d83a8b';
 //             let part = 'current';
 
-//             console.log(cities.findIndex(item => item.name === 'Minsk' ? true : false));
+//             //console.log(cities.findIndex(item => item.name === 'Minsk' ? true : false));
             
             
             
-            //const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=${part}&appid=${apiKey}`;
-            // const api = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=${apiKey}`;
-            // fetch(api)
-            //     .then(response => {
-            //         return response.json()
-            //     })
-            //     .then(data => {
-            //         console.log(data)
-            //     });
+//             const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=${part}&appid=${apiKey}`;
+//             //const api = `https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=${apiKey}`;
+//             fetch(api)
+//                 .then(response => {
+//                     return response.json()
+//                 })
+//                 .then(data => {
+//                     console.log(data)
+//                 });
 //         });
 //     }
 // });
